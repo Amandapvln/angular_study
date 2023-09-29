e3f2e3bca79c3efc45301d5189f2364c093e74d0
Amandapvln <amanda-lol@hotmail.com>
2023-09-29 20:49:00 -0300

    projeto calculadora

--- calculadora/src/app/app.module.ts
+++ calculadora/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-
+import { CalculadoraModule } from './calculadora';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculatorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    CalculadoraModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
