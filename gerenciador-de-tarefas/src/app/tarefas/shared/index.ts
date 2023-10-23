492a3fb981b62c82a26867f4d8b402eab6c4b8a3
Amandapvln <amanda-lol@hotmail.com>
2023-10-23 19:47:08 -0300

    atualização gerenciador de tarefas

--- gerenciador-de-tarefas/src/app/tarefas/shared/index.ts
+++ gerenciador-de-tarefas/src/app/tarefas/shared/index.ts
@@ -1,3 +1,2 @@
 export * from './tarefa.model';
 export * from './tarefa.service';
-export * from './tarefa-concluida.directive';
--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,30 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-import { 
-  TarefaService,
-  TarefaConcluidaDirective 
-} from './shared';
-import { ListarTarefaComponent } from './listar';
-import { CadastrarTarefaComponent } from './cadastrar';
-import { EditarTarefaComponent } from './editar';
+import { TarefaService } from './shared';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule
+  imports:[
+    CommonModule
   ],
-  declarations: [
-  	ListarTarefaComponent,
-  	CadastrarTarefaComponent,
-  	EditarTarefaComponent,
-  	TarefaConcluidaDirective
+  declarations:[
+
   ],
-  providers: [
-  	TarefaService
+  providers:[
+    TarefaService
   ]
 })
 export class TarefasModule { }
